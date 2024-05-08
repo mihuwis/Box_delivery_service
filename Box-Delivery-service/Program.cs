f70257f5612f82034256c5f305263e4df8ab6d65
mihuwis <mihuwis@gmail.com>
2024-05-08 22:20:44 +0200

    Add project and gitignore

---
+++ Box-Delivery-service/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Box_Delivery_service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
