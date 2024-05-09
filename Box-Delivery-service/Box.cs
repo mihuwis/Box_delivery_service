65523dca0f1530702b46372000646f71698b3f70
mihuwis <mihuwis@gmail.com>
2024-05-09 19:44:52 +0200

    Add Constructor with parameters

--- Box-Delivery-service/Box.cs
+++ Box-Delivery-service/Box.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,24 +7,47 @@ using System.Threading.Tasks;
 
 namespace Box_Delivery_service
 {
-    public sealed class Box
+    public sealed class Box : IEnumerable<double>, IEquatable<Box>
     {
         private readonly Double _width;
         private readonly Double _length;
         private readonly Double _height;
+        private readonly UnitOfMeasure unitOfMeasure;
+
+        public enum UnitOfMeasure
+        {
+            MILIMETER,
+            CENTIMETER,
+            METER
+        }
 
         public Box()
         {
 
         }
 
-        public Box(double width, double length, double height)
+        public Box(double width, double length, double height, UnitOfMeasure unit = UnitOfMeasure.METER)
         {
             if (width <= 0 || length <= 0 || height <= 0) throw new ArgumentOutOfRangeException("Dimensions need to be greater than Zero");
             if (width > 10 || length > 10 || height > 10) throw new ArgumentOutOfRangeException("Dimensions need to no biger than 10m");
             _width = width;
             _length = length;
             _height = height;
+        }
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IEquatable<Box>.Equals(Box? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
     }
 }
